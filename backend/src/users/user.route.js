08fc1ac15f01875e36ae7f5fda592a8b26ab0bde
Abdul Wahab Khan <asad49033@gmail.com>
2025-02-21 22:50:27 +0500

    hehehe

--- backend/src/users/user.route.js
+++ backend/src/users/user.route.js
@@ -48,4 +48,14 @@ router.get("/", async (req, res) => {
 }
 )
 
+
+router.patch("/test", async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+)
 module.exports = router;
